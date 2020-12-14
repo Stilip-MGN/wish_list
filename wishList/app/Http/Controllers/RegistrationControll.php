77cc3f6152b032ea8659d9b32f72b8c57edeed04
Andrew Krysh <vip.krysh@mail.ru>
2020-12-14 20:54:56 +0500

    исправил валидацию

--- wishList/app/Http/Controllers/RegistrationControll.php
+++ wishList/app/Http/Controllers/RegistrationControll.php
@@ -8,8 +8,8 @@ class RegistrationControll extends Controller
 {
     public function submit(Request $request ){
         $validation = $request->validate([
-            "name" => "required",
-            "password" => "required",
+            "name" => "required|min:4|max:15",
+            "password" => "required|min:6|max:15",
             "email" => "required",
         ]);
     }
--- wishList/app/Http/Controllers/SignControll.php
+++ wishList/app/Http/Controllers/SignControll.php
@@ -8,8 +8,8 @@ class SignControll extends Controller
 {
     public function submit(Request $request ){
         $validation = $request->validate([
-            "user_name" => "required|min:4|max:15",
-            "user_password" => "required|min:6|max:15"
+            "user_name" => "required",
+            "user_password" => "required"
         ]);
     }
 }
